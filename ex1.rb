db1fdf75c3aebe18d10bc95b5069c9cc7aaa3e07
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-23 16:53:55 +0530

    sec

--- ex1.rb
+++ ex1.rb
@@ -1,10 +1,11 @@
-def traverse
- puts "Enter the string"
- str = gets.chomp
- char_count = Hash.new(0)
- str.each_char do |c|
-   char_count[c] += 1
-   puts "#{c} : #{char_count}"
- end
+def traverseString
+  pattern = /^[A-z]+$/
+  puts "Enter the string"
+  str = gets.chomp
+  char_count = Hash.new(0)
+  str.each_char do |c|
+    char_count[c] += 1 if c =~ pattern 
+  end
+  puts char_count
 end
-traverse
+traverseString
