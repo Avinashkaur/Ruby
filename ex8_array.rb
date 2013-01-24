3902bbbd2b1f7282650b52e456d9e43869c9c325
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-24 17:38:42 +0530

    revised_ex8

--- ex8_array.rb
+++ ex8_array.rb
@@ -1,18 +1,14 @@
 class Array
-  attr_accessor :squared_array
   def power(x)
-    @@squared_array = []
+    new_array = []
     self.each { |i|
-     @@squared_array << i**x
+      new_array << i**x
     }
-    print " #{@@squared_array} \n"
+    print " #{new_array} \n"
   end
 end
-
 arr1 = Array.new
 arr1 = [1,2,3]
 arr1.power(2)
 arr1.power(3)
 arr1.power(4)
-
-
