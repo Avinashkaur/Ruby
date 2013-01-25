f8b79c84022990194e0f0c372e8620c63da340ab
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-25 18:23:56 +0530

    revised

--- ex3.rb
+++ ex3.rb
@@ -2,9 +2,6 @@ def replaceVowel
   pattern = /[aeiou]/
   puts "Enter a string:"
   str = gets.chomp
-  str.each_char do |i|
-    i = '*' if i =~ pattern  
-    print i 
-  end
+  puts str.gsub(pattern,'*')
 end
 replaceVowel
