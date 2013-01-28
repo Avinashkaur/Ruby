bfbf14dbe966db845d68509a571f8aa59e35c88e
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-28 14:30:12 +0530

    with_user_input

--- search_highlight.rb
+++ search_highlight.rb
@@ -1,12 +1,9 @@
 def highlight_pattern(user_string, searched_item)
-  puts user_string.gsub(searched_item, "(" + searched_item + ")")
-  puts "Occurences:  #{user_string.scan(searched_item).leyongth}"
+  puts user_string.gsub(/#{searched_item}/i, '(\0)')
+  puts "Occurences:  #{user_string.scan(/#{searched_item}/i).length}"
 end
-puts "enter inout string"
+puts "enter input string"
 user_string = gets.chomp
 puts "enter item to be searched"
 searched_item = gets.chomp
 highlight_pattern(user_string, searched_item)
-# highlight_pattern("Can you do me a favour.")
-# highlight_pattern("CANnes film festival is famous")
-# highlight_pattern("Can you can a can as a canner can can a can?")
