3644bbc1946181351c63d11f5103907055f2a2fa
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-28 13:10:31 +0530

    edge_case added

--- factorial.rb
+++ factorial.rb
@@ -1,5 +1,9 @@
 def factorial(upper_limit)
+  if (upper_limit == 0)
+    fact = 1
+  else 
   fact = (1..upper_limit).inject { |prod,i| prod *= i }
+  end
   puts "Factorial of #{upper_limit} is #{fact}"
 end
 puts "enter the upper limit:"
--- search_highlight.rb
+++ search_highlight.rb
@@ -1,8 +1,12 @@
-def highlight_pattern(user_string)
-  counter = 0
-  puts user_string.gsub(/(?<pattern>can)/i, '(\k<pattern>)')
-  puts "Occurences:  #{user_string.scan(/(?<pattern>can)/i).length}"
+def highlight_pattern(user_string, searched_item)
+  puts user_string.gsub(searched_item, "(" + searched_item + ")")
+  puts "Occurences:  #{user_string.scan(searched_item).leyongth}"
 end
-highlight_pattern("Can you do me a favour.")
-highlight_pattern("CANnes film festival is famous")
-highlight_pattern("Can you can a can as a canner can can a can?")
+puts "enter inout string"
+user_string = gets.chomp
+puts "enter item to be searched"
+searched_item = gets.chomp
+highlight_pattern(user_string, searched_item)
+# highlight_pattern("Can you do me a favour.")
+# highlight_pattern("CANnes film festival is famous")
+# highlight_pattern("Can you can a can as a canner can can a can?")
