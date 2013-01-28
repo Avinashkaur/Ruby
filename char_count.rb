64cb30658f7be41ce94a452a54a82d5377a0b01a
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-28 12:20:56 +0530

    indented

--- char_count.rb
+++ char_count.rb
@@ -1,17 +1,18 @@
 def char_count(input_string)
   lower_count, upper_count, digit_count, special_count = 0,0,0,0
   input_string.each_char do |character|
-      if ('a'..'z').include?(character)  
-        lower_count += 1 
-      elsif ('A'..'Z').include?(character) 
-        upper_count += 1 
-      elsif ('0'..'9').include?(character)  
-        digit_count += 1 
-      else special_count += 1
-      end
+    if ('a'..'z').include?(character)  
+      lower_count += 1 
+    elsif ('A'..'Z').include?(character) 
+      upper_count += 1 
+    elsif ('0'..'9').include?(character)  
+      digit_count += 1 
+    else special_count += 1
+    end
   end
   puts "Lowercase characters: #{lower_count} Uppercase characters: #{upper_count} digits: #{digit_count} special characters: #{special_count} "
 end
+
 puts "enter a string:"
 input_string = gets.chomp
 char_count(input_string.gsub(/\s/, ""))
