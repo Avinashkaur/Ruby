ba80ed2e1197180d87ae38801bc23cf518ef363f
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-28 12:18:14 +0530

    revised

--- ex14_reverse_sentence.rb
+++ ex14_reverse_sentence.rb
@@ -2,7 +2,7 @@ def reverse_sentence
   puts "Enter string:"
   sentence = gets.chomp
   words = sentence.split
-  print words.flatten.reverse.join(" ")
+  print words.reverse.join(" ")
   print "\n"
 end
 reverse_sentence
