85fc54ec76c5225d05430746a7a9016258341e00
Avinash kaur <avinash.kaur@vinsol.com>
2013-01-31 13:38:16 +0530

    revised_csv

--- csv/csv.rb
+++ csv/csv.rb
@@ -1,22 +1,19 @@
 def load_file
-
   profiles = Hash.new { |hash, key| hash[key] = []  }
-  File.open("emp.csv", "r") do |f|
-    f.each_line do |line|
-      records = line.split(",").each { |entry| entry.chomp!}
-      profiles[records[2]].push("#{records[0]} (Emp Id: #{records[1]})")
-    end
+  IO.foreach("emp.csv") do |line|
+    records = line.split(",").each { |entry| entry.chomp! }
+    profiles[records[2]].push("#{records[0]} (Emp Id: #{records[1]})")
   end
   write_file(profiles)
 end
 
 def write_file(profiles)
-  File.open("designations.csv", "w") do |f|
-    profiles.each_key do |keyvalue|
-      f.puts keyvalue
-      profiles.each_pair do |k, v|
-        if k == keyvalue
-          f.puts(v)
+  File.open("designations.csv", "w") do |line|
+    profiles.each_key do |keyname|
+      line.puts keyname
+      profiles.each_pair do |key, value|
+        if key == keyname
+          line.puts(value)
         end
       end
     end
