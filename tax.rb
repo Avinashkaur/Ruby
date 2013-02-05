9eb419238b4576cf17b0229fac049a428aa21b7f
Avinash kaur <avinash.kaur@vinsol.com>
2013-02-05 11:51:51 +0530

    tax_updated

--- tax.rb
+++ tax.rb
@@ -1,18 +1,14 @@
 class Products
   attr_accessor:productname, :imported, :exempted, :price
-  @@total = 0
   def initialize(productname,imported,exempted,price)
     @productname = productname
     @imported = imported
     @exempted = exempted
     @price = price
-    importduty,salestax = 0.0,0.0
+    importduty,salestax = 0.0, 0.0
     importduty = import_duty(@price) if @imported =~ /yes/i
     salestax = sales_tax(@price) if @exempted =~ /no/i
     @price += importduty + salestax
-  end
-  def calculate_tax
-    @@total += @price.to_i.round
   end
   def import_duty(itemprice)
     0.05*itemprice.to_f
@@ -41,7 +37,7 @@ class Productlist
       puts "Exempted: #{product.exempted}"
       puts "Price: #{product.price.round(2)}"
       puts "-" *30
-      total = product.calculate_tax
+      total += product.price
     end
     puts "Total Bill: #{total}"
   end
